d214745b5e196c23a7129a559cd7c065835239cb
Nguyễn Ngọc Nguyên <gpk.bluesky97@gmail.com>
2021-04-07 10:01:38 +0700

    [feat]: form tạo nhân viên - clone ERP Trần

--- src/pages/object-profiles/utils/variables.js
+++ src/pages/object-profiles/utils/variables.js
@@ -13,7 +13,47 @@ export const variables = {
     VALID: 'Lỗi',
     EXPIRE: 'Nghỉ học',
   },
+  TYPE_USER: {
+    RANK: 'rank',
+    BANK: 'bank',
+    INFO: 'info',
+    LEVEL: 'level',
+    CONFIG: 'config',
+    HEALTH: 'health',
+    SALARY: 'salary',
+    REWARD: 'reward',
+    HISTORY: 'history',
+    CONTACT: 'contact',
+    DAYS_OFF: 'daysOff',
+    CONTRACT: 'contract',
+    CHILDREN: 'children',
+    TIME_WORK: 'timeWork',
+    INSURRANCE: 'insurrance',
+    CERTIFICAIE: 'certificate',
+    MAGNETIC_CARDS: 'magnetic-cards',
+    INSURRANCEHEALTH: 'InsurranceHealth',
+    MINUTEST_OF_AGREEMENT: 'minutes-of-agreement',
+  },
+  LIST_PAYMENTS: [
+    {
+      value: 'CASH',
+      name: 'Tiền mặt',
+    },
+    {
+      value: 'TRANSFER',
+      name: 'Chuyển khoản',
+    },
+  ],
+  LIST_STATUS_PAYMENTS: [
+    {
+      value: 'APPLYING',
+      name: 'Đang áp dụng',
+    },
+    {
+      value: 'EXPIRED',
+      name: 'Hết hiệu lực',
+    },
+  ],
 };
 
 export default variables;
- 
