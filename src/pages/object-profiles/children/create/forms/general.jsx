a2e0c6e2d814f22be09ee13522574ca906704768
PhuTV <phutv@greenglobal.vn>
2021-04-14 14:58:30 +0700

    Update model class

--- src/pages/object-profiles/children/create/forms/general.jsx
+++ src/pages/object-profiles/children/create/forms/general.jsx
@@ -19,217 +19,252 @@ const mapStateToProps = ({ loading, OPchildrenAdd }) => ({
   loading,
   details: OPchildrenAdd.details,
   error: OPchildrenAdd.error,
+  branches: OPchildrenAdd.branches,
+  classes: OPchildrenAdd.classes,
 });
-const General = memo(({ dispatch, loading: { effects }, match: { params }, details, error }) => {
-  const formRef = useRef();
-  const [fileImage, setFileImage] = useState(null);
-  const [dayOfBirth, setDayOfBirth] = useState(null);
-  const mounted = useRef(false);
-  const mountedSet = (setFunction, value) =>
-    !!mounted?.current && setFunction && setFunction(value);
+const General = memo(
+  ({ dispatch, loading: { effects }, match: { params }, details, error, branches, classes }) => {
+    const formRef = useRef();
+    const [fileImage, setFileImage] = useState(null);
+    const [dayOfBirth, setDayOfBirth] = useState(null);
+    const mounted = useRef(false);
+    const mountedSet = (setFunction, value) =>
+      !!mounted?.current && setFunction && setFunction(value);
 
-  const loadingSubmit = effects[`OPchildrenAdd/ADD`] || effects[`OPchildrenAdd/UPDATE`];
-  const loading = effects[`OPchildrenAdd/GET_DETAILS`];
+    const loadingSubmit = effects[`OPchildrenAdd/ADD`] || effects[`OPchildrenAdd/UPDATE`];
+    const loading = effects[`OPchildrenAdd/GET_DETAILS`];
 
-  const onChaneDate = (e) => {
-    mountedSet(setDayOfBirth, e);
-  };
+    const onChaneDate = (e) => {
+      mountedSet(setDayOfBirth, e);
+    };
 
-  /**
-   * Function submit form modal
-   * @param {object} values values of form
-   */
-  const onFinish = (values) => {
-    const age = moment().endOf('month').diff(moment(values.dayOfBirth).startOf('month'), 'month');
-    dispatch({
-      type: params.id ? 'OPchildrenAdd/UPDATE' : 'OPchildrenAdd/ADD',
-      payload: params.id
-        ? {
-            ...details,
-            id: params.id,
-            student: {
-              ...details.student,
-              ...values,
-              id: params.id,
-              fileImage,
-              age,
-            },
-          }
-        : {
-            student: {
-              ...values,
-              fileImage,
-              age,
-            },
-          },
-      callback: (response, error) => {
-        if (response) {
-          history.push(`/ho-so-doi-tuong/hoc-sinh/${response?.student?.id}/chi-tiet?type=general`);
-        }
-        if (error) {
-          if (error?.validationErrors && !isEmpty(error?.validationErrors)) {
-            error?.validationErrors.forEach((item) => {
-              formRef.current.setFields([
-                {
-                  name: head(item.members),
-                  errors: [item.message],
-                },
-              ]);
-            });
-          }
-        }
-      },
-    });
-  };
-
-  useEffect(() => {
-    if (params.id) {
+    const onChangeBranch = (e) => {
       dispatch({
-        type: 'OPchildrenAdd/GET_DETAILS',
+        type: 'OPchildrenAdd/GET_CLASSES',
+        payload: {
+          branch: e,
+        },
+      });
+    };
+
+    /**
+     * Function submit form modal
+     * @param {object} values values of form
+     */
+    const onFinish = (values) => {
+      const age = moment().endOf('month').diff(moment(values.dayOfBirth).startOf('month'), 'month');
+      dispatch({
+        type: params.id ? 'OPchildrenAdd/UPDATE' : 'OPchildrenAdd/ADD',
+        payload: params.id
+          ? {
+              ...details,
+              id: params.id,
+              student: {
+                ...details.student,
+                ...values,
+                id: params.id,
+                fileImage,
+                age,
+              },
+            }
+          : {
+              student: {
+                ...values,
+                fileImage,
+                age,
+              },
+            },
+        callback: (response, error) => {
+          if (response) {
+            history.push(
+              `/ho-so-doi-tuong/hoc-sinh/${response?.student?.id}/chi-tiet?type=general`,
+            );
+          }
+          if (error) {
+            if (error?.validationErrors && !isEmpty(error?.validationErrors)) {
+              error?.validationErrors.forEach((item) => {
+                formRef.current.setFields([
+                  {
+                    name: head(item.members),
+                    errors: [item.message],
+                  },
+                ]);
+              });
+            }
+          }
+        },
+      });
+    };
+
+    useEffect(() => {
+      if (params.id) {
+        dispatch({
+          type: 'OPchildrenAdd/GET_DETAILS',
+          payload: params,
+        });
+      }
+    }, [params.id]);
+
+    useEffect(() => {
+      dispatch({
+        type: 'OPchildrenAdd/GET_BRANCHES',
         payload: params,
       });
-    }
-  }, [params.id]);
+    }, []);
 
-  useEffect(() => {
-    mounted.current = true;
-    return () => (mounted.current = false);
-  }, []);
+    useEffect(() => {
+      mounted.current = true;
+      return () => (mounted.current = false);
+    }, []);
 
-  useEffect(() => {
-    if (!isEmpty(details) && params.id) {
-      formRef.current.setFieldsValue({
-        ...details.student,
-        dayOfBirth: details?.student?.dayOfBirth && moment(details?.student?.dayOfBirth),
-        registerDate: details?.student?.registerDate && moment(details?.student?.registerDate),
-      });
-      mountedSet(setDayOfBirth(moment(details?.student?.dayOfBirth)));
-      mountedSet(setFileImage, details?.student?.fileImage);
-    }
-  }, [details]);
+    useEffect(() => {
+      if (!isEmpty(details) && params.id) {
+        formRef.current.setFieldsValue({
+          ...details.student,
+          dayOfBirth: details?.student?.dayOfBirth && moment(details?.student?.dayOfBirth),
+          registerDate: details?.student?.registerDate && moment(details?.student?.registerDate),
+          branchId: details?.class?.branchId,
+        });
+        mountedSet(setDayOfBirth(moment(details?.student?.dayOfBirth)));
+        mountedSet(setFileImage, details?.student?.fileImage);
+      }
+    }, [details]);
 
-  return (
-    <Form layout="vertical" ref={formRef} onFinish={onFinish}>
-      <Pane className="card">
-        <Loading loading={loading} isError={error.isError} params={{ error }}>
-          <Pane style={{ padding: 20 }} className="pb-0 border-bottom">
-            <Heading type="form-title" style={{ marginBottom: 20 }}>
-              Thông tin cơ bản
-            </Heading>
-            <Pane className="row">
-              <Pane className="col">
-                <Form.Item name="avatar" label="Hình ảnh học sinh">
-                  <ImageUpload
-                    callback={(res) => {
-                      mountedSet(setFileImage, res.fileInfo.url);
-                    }}
+    return (
+      <Form layout="vertical" ref={formRef} onFinish={onFinish}>
+        <Pane className="card">
+          <Loading loading={loading} isError={error.isError} params={{ error }}>
+            <Pane style={{ padding: 20 }} className="pb-0 border-bottom">
+              <Heading type="form-title" style={{ marginBottom: 20 }}>
+                Thông tin cơ bản
+              </Heading>
+              <Pane className="row">
+                <Pane className="col">
+                  <Form.Item name="avatar" label="Hình ảnh học sinh">
+                    <ImageUpload
+                      callback={(res) => {
+                        mountedSet(setFileImage, res.fileInfo.url);
+                      }}
+                    />
+                  </Form.Item>
+                </Pane>
+              </Pane>
+
+              <Pane className="row">
+                <Pane className="col-lg-4">
+                  <FormItem
+                    name="fullName"
+                    label="Họ và tên"
+                    type={variables.INPUT}
+                    rules={[variables.RULES.EMPTY_INPUT, variables.RULES.MAX_LENGTH_INPUT]}
                   />
-                </Form.Item>
+                </Pane>
+                <Pane className="col-lg-4">
+                  <FormItem
+                    name="code"
+                    label="Mã học sinh"
+                    type={variables.INPUT}
+                    rules={[variables.RULES.EMPTY_INPUT, variables.RULES.MAX_LENGTH_INPUT]}
+                  />
+                </Pane>
+                <Pane className="col-lg-4">
+                  <FormItem
+                    name="registerDate"
+                    label="Ngày vào lớp"
+                    type={variables.DATE_PICKER}
+                    rules={[variables.RULES.EMPTY]}
+                  />
+                </Pane>
+
+                <Pane className="col-lg-4">
+                  <FormItem
+                    name="dayOfBirth"
+                    label="Ngày sinh"
+                    type={variables.DATE_PICKER}
+                    rules={[variables.RULES.EMPTY]}
+                    onChange={onChaneDate}
+                  />
+                </Pane>
+                <Pane className="col-lg-4">
+                  <Form.Item label="Tuổi(tháng)">
+                    {dayOfBirth &&
+                      moment().endOf('month').diff(moment(dayOfBirth).startOf('month'), 'month')}
+                  </Form.Item>
+                </Pane>
+                <Pane className="col-lg-4">
+                  <FormItem
+                    data={genders}
+                    name="sex"
+                    label="Giới tính"
+                    type={variables.SELECT}
+                    rules={[variables.RULES.EMPTY]}
+                  />
+                </Pane>
+                <Pane className="col-lg-4">
+                  <FormItem
+                    data={branches}
+                    name="branchId"
+                    label="Mã cơ sở"
+                    type={variables.SELECT}
+                    rules={[variables.RULES.EMPTY]}
+                    onChange={onChangeBranch}
+                  />
+                </Pane>
+                <Pane className="col-lg-4">
+                  <FormItem
+                    data={classes}
+                    name="classId"
+                    label="Mã lớp"
+                    type={variables.SELECT}
+                    rules={[variables.RULES.EMPTY]}
+                  />
+                </Pane>
+                <Pane className="col-lg-4">
+                  <FormItem data={[]} name="yearId" label="Mã năm" type={variables.SELECT} />
+                </Pane>
+              </Pane>
+
+              <Heading type="form-block-title" style={{ marginBottom: 12 }}>
+                Địa chỉ
+              </Heading>
+
+              <Pane className="row">
+                <Pane className="col-lg-6">
+                  <FormItem name="address" label="Số nhà" type={variables.INPUT} />
+                </Pane>
+                <Pane className="col-lg-6">
+                  <FormItem name="street" label="Tên đường" type={variables.INPUT} />
+                </Pane>
+              </Pane>
+
+              <Pane className="row">
+                <Pane className="col-lg-12">
+                  <FormItem
+                    data={[]}
+                    name="wardId"
+                    label="Thành Phố/Quận Huyện"
+                    type={variables.CASCADER}
+                  />
+                </Pane>
               </Pane>
             </Pane>
 
-            <Pane className="row">
-              <Pane className="col-lg-4">
-                <FormItem
-                  name="fullName"
-                  label="Họ và tên"
-                  type={variables.INPUT}
-                  rules={[variables.RULES.EMPTY_INPUT, variables.RULES.MAX_LENGTH_INPUT]}
-                />
-              </Pane>
-              <Pane className="col-lg-4">
-                <FormItem
-                  name="code"
-                  label="Mã học sinh"
-                  type={variables.INPUT}
-                  rules={[variables.RULES.EMPTY_INPUT, variables.RULES.MAX_LENGTH_INPUT]}
-                />
-              </Pane>
-              <Pane className="col-lg-4">
-                <FormItem
-                  name="registerDate"
-                  label="Ngày vào lớp"
-                  type={variables.DATE_PICKER}
-                  rules={[variables.RULES.EMPTY]}
-                />
-              </Pane>
-
-              <Pane className="col-lg-4">
-                <FormItem
-                  name="dayOfBirth"
-                  label="Ngày sinh"
-                  type={variables.DATE_PICKER}
-                  rules={[variables.RULES.EMPTY]}
-                  onChange={onChaneDate}
-                />
-              </Pane>
-              <Pane className="col-lg-4">
-                <Form.Item label="Tuổi(tháng)">
-                  {dayOfBirth &&
-                    moment().endOf('month').diff(moment(dayOfBirth).startOf('month'), 'month')}
-                </Form.Item>
-              </Pane>
-              <Pane className="col-lg-4">
-                <FormItem
-                  data={genders}
-                  name="sex"
-                  label="Giới tính"
-                  type={variables.SELECT}
-                  rules={[variables.RULES.EMPTY]}
-                />
-              </Pane>
-              <Pane className="col-lg-4">
-                <FormItem data={[]} name="classId" label="Mã lớp" type={variables.SELECT} />
-              </Pane>
-
-              <Pane className="col-lg-4">
-                <FormItem data={[]} name="premisesId" label="Mã cơ sở" type={variables.SELECT} />
-              </Pane>
-              <Pane className="col-lg-4">
-                <FormItem data={[]} name="yearId" label="Mã năm" type={variables.SELECT} />
-              </Pane>
+            <Pane style={{ padding: 20 }}>
+              <Button
+                color="success"
+                size="large"
+                htmlType="submit"
+                style={{ marginLeft: 'auto' }}
+                loading={loadingSubmit}
+              >
+                Lưu
+              </Button>
             </Pane>
-
-            <Heading type="form-block-title" style={{ marginBottom: 12 }}>
-              Địa chỉ
-            </Heading>
-
-            <Pane className="row">
-              <Pane className="col-lg-6">
-                <FormItem name="address" label="Số nhà" type={variables.INPUT} />
-              </Pane>
-              <Pane className="col-lg-6">
-                <FormItem name="street" label="Tên đường" type={variables.INPUT} />
-              </Pane>
-            </Pane>
-
-            <Pane className="row">
-              <Pane className="col-lg-12">
-                <FormItem
-                  data={[]}
-                  name="wardId"
-                  label="Thành Phố/Quận Huyện"
-                  type={variables.CASCADER}
-                />
-              </Pane>
-            </Pane>
-          </Pane>
-
-          <Pane style={{ padding: 20 }}>
-            <Button
-              color="success"
-              size="large"
-              htmlType="submit"
-              style={{ marginLeft: 'auto' }}
-              loading={loadingSubmit}
-            >
-              Lưu
-            </Button>
-          </Pane>
-        </Loading>
-      </Pane>
-    </Form>
-  );
-});
+          </Loading>
+        </Pane>
+      </Form>
+    );
+  },
+);
 
 export default withRouter(connect(mapStateToProps)(General));
