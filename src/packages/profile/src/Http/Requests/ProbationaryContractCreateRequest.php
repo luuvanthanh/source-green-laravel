45e1fc0bfd315ad7a2f576c613ce16149130f0c3
sieudd <sieudd@greenglobal.vn>
2021-04-20 08:26:01 +0700

    edit validate profile

--- src/packages/profile/src/Http/Requests/ProbationaryContractCreateRequest.php
+++ src/packages/profile/src/Http/Requests/ProbationaryContractCreateRequest.php
@@ -27,16 +27,16 @@ class ProbationaryContractCreateRequest extends FormRequest
             'employeeId' => 'required|exists:Employees,Id',
             'contractNumber' => 'required|string',
             'contractDate' => 'required|date',
-            'typeOfContractId' => 'required|exists:type_of_contracts,Id',
+            'typeOfContractId' => 'required|exists:TypeOfContracts,Id',
             'salaryRatio' => 'required',
             'month' => 'required',
-            'divisionId' => 'required|exists:divisions,Id',
+            'divisionId' => 'required|exists:Divisions,Id',
             'contractFrom' => 'required|date',
             'contractTo' => 'required|date',
-            'positionId' => 'required|exists:positions,Id',
+            'positionId' => 'required|exists:Positions,Id',
             'work' => 'required|string',
             'workTime' => 'required|string',
-            'branchId' => 'required|exists:branches,Id',
+            'branchId' => 'required|exists:Branches,Id',
         ];
     }
 }
