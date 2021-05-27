3c8823dcb4662487eaeec9ed2d5a111378c95563
PhuTV <phutv@greenglobal.vn>
2021-05-27 11:39:19 +0700

    Implement modules work bus

--- src/pages/hrm/works/bus/services.js
+++ src/pages/hrm/works/bus/services.js
@@ -2,7 +2,7 @@ import request from '@/utils/requestLavarel';
 import { Helper, variables } from '@/utils';
 
 export function get(data = {}) {
-  return request('/v1/timekeeping-report', {
+  return request('/v1/bus-registrations-summary', {
     method: 'GET',
     params: {
       limit: data.limit,
