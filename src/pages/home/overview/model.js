11c3f2270bde6dfe28269db21f2d63b7d8861cf3
Nguyen Ngoc Dien <diennn@greenglobal.vn>
2021-05-27 08:09:20 +0700

    integrate api for bus in overview

--- src/pages/home/overview/model.js
+++ src/pages/home/overview/model.js
@@ -1,4 +1,5 @@
 import * as services from './services';
+import variablesModules from '../variables';
 
 export default {
   namespace: 'overView',
@@ -8,6 +9,11 @@ export default {
     medicals: [],
     detailsMedical: {},
     medicalsStudent: [],
+    bus: [],
+    listBusByStatus: {
+      data: [],
+      pagination: {}
+    },
   },
   reducers: {
     INIT_STATE: (state) => ({ ...state, isError: false, data: [] }),
@@ -30,6 +36,17 @@ export default {
     SET_LIST_MEDICAL_BY_STUDENT: (state, { payload }) => ({
       ...state,
       medicalsStudent: payload,
+    }),
+    SET_DATA_BUS: (state, { payload }) => ({
+      ...state,
+      bus: variablesModules.DATA_BUS || payload,
+    }),
+    SET_DATA_BUS_BY_STATUS: (state, { payload }) => ({
+      ...state,
+      listBusByStatus: {
+        data: [{ id: 1 }] || payload.parsePayload,
+        pagination: payload.pagination,
+      },
     }),
   },
   effects: {
@@ -87,6 +104,33 @@ export default {
         yield saga.put({
           type: 'SET_LIST_MEDICAL_BY_STUDENT',
           payload: response,
+        });
+      } catch (error) {
+        // continue regardless of error
+      }
+    },
+    *GET_DATA_BUS({ payload }, saga) {
+      try {
+        const response = yield saga.call(services.getBus, payload);
+        yield saga.put({
+          type: 'SET_DATA_BUS',
+          payload: response,
+        });
+      } catch (error) {
+        // continue regardless of error
+      }
+    },
+    *GET_DATA_BUS_BY_STATUS({ payload }, saga) {
+      try {
+        const response = yield saga.call(services.getBusByStatus, payload);
+        yield saga.put({
+          type: 'SET_DATA_BUS_BY_STATUS',
+          payload: {
+            parsePayload: response?.items,
+            pagination: {
+              total: response?.totalCount || 0,
+            },
+          },
         });
       } catch (error) {
         // continue regardless of error
