cbf82a87f85a0b1a9098012cc0cfb3825327dc27
foxymen9 <foxymen9@gmail.com>
2021-06-10 17:03:35 +0700

    Implement modules tool groups

---
+++ src/pages/criteria/teaching-tools/angle-tool/create/model.js
@@ -0,0 +1,99 @@
+import { notification } from 'antd';
+import { get } from 'lodash';
+import * as services from './services';
+
+export default {
+  namespace: 'criteriaAngleToolCreate',
+  state: {
+    details: {},
+    error: {
+      isError: false,
+      data: {},
+    },
+    toolDetails: [],
+  },
+  reducers: {
+    INIT_STATE: (state) => ({ ...state, isError: false, data: [] }),
+    SET_DATA: (state, { payload }) => ({
+      ...state,
+      details: payload,
+    }),
+    SET_TOOL_DETAILS: (state, { payload }) => ({
+      ...state,
+      toolDetails: payload.items,
+    }),
+    SET_ERROR: (state, { payload }) => ({
+      ...state,
+      error: {
+        isError: true,
+        data: {
+          ...payload,
+        },
+      },
+    }),
+  },
+  effects: {
+    *GET_TOOL_DETAILS({ payload }, saga) {
+      try {
+        const response = yield saga.call(services.getToolDetails, payload);
+        yield saga.put({
+          type: 'SET_TOOL_DETAILS',
+          payload: response,
+        });
+      } catch (error) {
+        yield saga.put({
+          type: 'SET_ERROR',
+          payload: error.data,
+        });
+      }
+    },
+    *GET_DATA({ payload, callback }, saga) {
+      try {
+        const response = yield saga.call(services.get, payload);
+        callback(response);
+      } catch (error) {
+        callback(null, error);
+        yield saga.put({
+          type: 'SET_ERROR',
+          payload: error.data,
+        });
+      }
+    },
+    *ADD({ payload, callback }, saga) {
+      try {
+        yield saga.call(services.add, payload);
+        callback(payload);
+        notification.success({
+          message: 'THÔNG BÁO',
+          description: 'Dữ liệu cập nhật thành công',
+        });
+      } catch (error) {
+        if (get(error.data, 'error.validationErrors[0]')) {
+          notification.error({
+            message: 'THÔNG BÁO',
+            description: get(error.data, 'error.validationErrors[0].message'),
+          });
+        }
+        callback(null, error?.data?.error);
+      }
+    },
+    *UPDATE({ payload, callback }, saga) {
+      try {
+        yield saga.call(services.update, payload);
+        callback(payload);
+        notification.success({
+          message: 'THÔNG BÁO',
+          description: 'Dữ liệu cập nhật thành công',
+        });
+      } catch (error) {
+        if (get(error.data, 'error.validationErrors[0]')) {
+          notification.error({
+            message: 'THÔNG BÁO',
+            description: get(error.data, 'error.validationErrors[0].message'),
+          });
+        }
+        callback(null, error?.data?.error);
+      }
+    },
+  },
+};
