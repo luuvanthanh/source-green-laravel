ec8b85b2f3c5c055e44643b13ef91c7506ca1fb3
foxymen9 <foxymen9@gmail.com>
2021-06-10 15:47:40 +0700

    Implement modules tool-details

--- src/pages/criteria/teaching-tools/tool/create/index.jsx
+++ src/pages/criteria/teaching-tools/tool/create/index.jsx
@@ -1,10 +1,9 @@
-import { memo, useRef, useEffect, useState } from 'react';
+import { memo, useRef, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { Form } from 'antd';
 import { useSelector, useDispatch } from 'dva';
-import { useHistory } from 'umi';
-import { v4 as uuidv4 } from 'uuid';
-import _ from 'lodash';
+import { useHistory, useParams } from 'umi';
+import { head, isEmpty } from 'lodash';
 
 import Breadcrumbs from '@/components/LayoutComponents/Breadcrumbs';
 import Pane from '@/components/CommonComponent/Pane';
@@ -14,86 +13,76 @@ import FormItem from '@/components/CommonComponent/FormItem';
 import Text from '@/components/CommonComponent/Text';
 import { variables } from '@/utils';
 
-const levelsDefault = [
-  {
-    name: '1',
-    childs: [
-      {
-        id: uuidv4(),
-        value: ''
-      },
-      {
-        id: uuidv4(),
-        value: ''
-      }
-    ],
-
-  },
-  {
-    name: '2',
-    childs: [
-      {
-        id: uuidv4(),
-        value: ''
-      },
-    ],
-
-  },
-  {
-    name: '3',
-    childs: [
-      {
-        id: uuidv4(),
-        value: ''
-      },
-    ],
-  },
-  {
-    name: '4',
-    childs: [
-      {
-        id: uuidv4(),
-        value: '123213'
-      },
-    ]
-  },
-]
-
-const Index = memo(({ }) => {
-  const [levels, setLevels] = useState(levelsDefault);
-
-  const { loading } = useSelector(({ loading }) => ({ loading }));
-  const [{ menuLeftCriteria }] = useSelector(({ menu }) => [menu]);
+const Index = memo(() => {
+  const [{ menuLeftCriteria }, loading] = useSelector(({ menu, loading: { effects } }) => [
+    menu,
+    effects,
+  ]);
   const dispatch = useDispatch();
+  const params = useParams();
 
   const history = useHistory();
   const formRef = useRef();
   const mounted = useRef(false);
-  const mountedSet = (action, value) => mounted?.current && action(value);
+  // const mountedSet = (action, value) => mounted?.current && action(value);
 
   const onFinish = (values) => {
-    history.goBack()
+    dispatch({
+      type: params.id ? 'criteriaToolCreate/UPDATE' : 'criteriaToolCreate/ADD',
+      payload: {
+        ...values,
+        ...params,
+        isFeedback: !!values.isFeedback,
+        toolLevels: values.toolLevels.map((item, index) => ({
+          ...item,
+          level: index + 1,
+          evaluates: item.evaluates.map((item) => item.name),
+        })),
+      },
+      callback: (response, error) => {
+        if (response) {
+          history.goBack();
+        }
+        if (error) {
+          if (error?.validationErrors && !isEmpty(error?.validationErrors)) {
+            error?.validationErrors.forEach((item) => {
+              formRef.current.setFields([
+                {
+                  name: head(item.members),
+                  errors: [item.message],
+                },
+              ]);
+            });
+          }
+        }
+      },
+    });
   };
 
   useEffect(() => {
+    dispatch({
+      type: 'criteriaToolCreate/GET_DATA',
+      payload: {
+        ...params,
+      },
+      callback: (response) => {
+        if (response) {
+          formRef.current.setFieldsValue({
+            ...response,
+            toolLevels: response.toolLevels.map((item) => ({
+              ...item,
+              evaluates: item.evaluate.map((item) => ({ name: item })),
+            })),
+          });
+        }
+      },
+    });
+  }, [params.id]);
+
+  useEffect(() => {
     mounted.current = true;
-    return () => (mounted.current = false);
+    return mounted.current;
   }, []);
-
-  const handleCreateCriteria = (index) => {
-    const newLevels = [...levels];
-    newLevels[index]['childs'].push({
-      id: uuidv4(),
-      value: ''
-    })
-    setLevels(newLevels);
-  }
-
-  const handleDelete = (index) => {
-    const newLevels = [...levels];
-    newLevels.splice(index, 1);
-    setLevels(newLevels);
-  }
 
   return (
     <Pane style={{ padding: 20, paddingBottom: 0 }}>
@@ -105,7 +94,9 @@ const Index = memo(({ }) => {
             layout="vertical"
             ref={formRef}
             onFinish={onFinish}
-            initialValues={{}}
+            initialValues={{
+              toolLevels: [{ evaluates: [{}] }],
+            }}
           >
             <Pane className="my20 mb0 card">
               <Pane className="border-bottom p20">
@@ -115,57 +106,95 @@ const Index = memo(({ }) => {
                 <FormItem
                   className="mb0"
                   label="Tên giáo cụ"
-                  name="tenGiaoCu"
+                  name="name"
                   type={variables.INPUT}
-                  // rules={[variables.RULES.EMPTY]}
+                  rules={[variables.RULES.EMPTY]}
                 />
               </Pane>
               <Pane className="border-bottom p20">
                 <Heading type="form-title" className="mb10">
                   Cấp độ
                 </Heading>
-                {
-                  levels.map((item, index) => (
-                    <Pane className={index === 0 ? 'mt10' : 'mt20'} key={index}>
-                      <Pane className="d-flex">
-                        <Text size="medium" className="fw-bold mr5 font-size-13">{item.name}.</Text>
-                        {index !== 0 && (
-                          <p
-                            onClick={() => handleDelete(index)}
-                            className="btn-delete font-size-13 fw-normal"
-                          >
-                            Xóa
-                          </p>
-                        )}
-                      </Pane>
-                      <Text size="normal" >Chương trình học</Text>
-                      {
-                        item?.childs.map((itemChild, indexChild) => (
-                          <Pane key={indexChild} className="mt10 d-flex justify-content-between align-items-center groups-input">
-                            <FormItem
-                              className="mb0"
-                              value={itemChild?.value}
-                              // name={`huongDan${item.name}${indexChild}`}
-                              name={[`test${item.name}`, `value${indexChild}`]}
-                              fieldKey={[`test${item.name}`, `value${indexChild}`]}
-                              type={variables.INPUT}
-                            />
-                            {indexChild !== 0 && (<span className="icon icon-remove" />)}
+                <Form.List name="toolLevels">
+                  {(fields, { add, remove }) => (
+                    <Pane>
+                      {fields.map((field, index) => (
+                        <Pane key={index}>
+                          <Pane className="d-flex mt10">
+                            <Text size="medium" className="fw-bold mr5 font-size-13">
+                              {index + 1}.
+                            </Text>
+                            {fields.length > 1 && (
+                              <p
+                                onClick={() => remove(field.name)}
+                                role="presentation"
+                                className="btn-delete font-size-13 fw-normal"
+                              >
+                                Xóa
+                              </p>
+                            )}
                           </Pane>
-                        ))
-                      }
+                          <Text size="normal">Tiêu chí đánh giá</Text>
+                          <Form.List
+                            fieldKey={[field.fieldKey, 'evaluates']}
+                            name={[field.name, 'evaluates']}
+                          >
+                            {(fieldsContent, { add, remove }) => (
+                              <Pane>
+                                {fieldsContent.map((fieldContent, indexContent) => (
+                                  <Pane
+                                    key={indexContent}
+                                    className="mt10 d-flex justify-content-between align-items-center groups-input"
+                                  >
+                                    <FormItem
+                                      className="mb0"
+                                      fieldKey={[fieldContent.fieldKey, 'name']}
+                                      name={[fieldContent.name, 'name']}
+                                      type={variables.INPUT}
+                                      rules={[variables.RULES.EMPTY]}
+                                    />
+                                    {fieldsContent.length > 1 && (
+                                      <span
+                                        className="icon icon-remove"
+                                        role="presentation"
+                                        onClick={() => remove(fieldContent.name)}
+                                      />
+                                    )}
+                                  </Pane>
+                                ))}
+                                <Pane className="mt10 d-flex align-items-center color-success pointer">
+                                  <span className="icon-plus-circle mr5" />
+                                  <span
+                                    onClick={() => add()}
+                                    role="presentation"
+                                    className="text-uppercase font-size-13"
+                                  >
+                                    Thêm tiêu chí
+                                  </span>
+                                </Pane>
+                              </Pane>
+                            )}
+                          </Form.List>
+                        </Pane>
+                      ))}
                       <Pane className="mt10 d-flex align-items-center color-success pointer">
                         <span className="icon-plus-circle mr5" />
-                        <span onClick={() => handleCreateCriteria(index)} className="text-uppercase font-size-13">Thêm tiêu chí</span>
+                        <span
+                          onClick={() => add()}
+                          role="presentation"
+                          className="text-uppercase font-size-13"
+                        >
+                          Thêm cấp độ
+                        </span>
                       </Pane>
                     </Pane>
-                  ))
-                }
+                  )}
+                </Form.List>
               </Pane>
               <Pane className="border-bottom p20 d-flex align-items-center">
                 <FormItem
                   className="mb0 mr10"
-                  name="isHasNote"
+                  name="isFeedback"
                   type={variables.SWITCH}
                   valuePropName="checked"
                 />
@@ -178,6 +207,9 @@ const Index = memo(({ }) => {
                   color="success"
                   htmlType="submit"
                   size="large"
+                  loading={
+                    loading['criteriaToolCreate/ADD'] || loading['criteriaToolCreate/UPDATE']
+                  }
                 >
                   Lưu
                 </Button>
