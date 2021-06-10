923e89620102b98e27e630f4406391aafcb0718b
Nguyen Ngoc Dien <diennn@greenglobal.vn>
2021-06-10 17:13:55 +0700

    Update no data in blocks overview

--- src/pages/home/overview/noteComponent.jsx
+++ src/pages/home/overview/noteComponent.jsx
@@ -7,6 +7,7 @@ import _ from 'lodash';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 
+import Empty from '@/components/CommonComponent/Table/Empty';
 import { Helper, variables } from '@/utils';
 import AvatarTable from '@/components/CommonComponent/AvatarTable';
 
@@ -189,7 +190,9 @@ const Index = memo(({ classId }) => {
           <Scrollbars autoHeight autoHeightMax={window.innerHeight - 355}>
             <div className="px20">
               {!loading['overView/GET_DATA_NOTE'] && _.isEmpty(notes) && (
-                <p className="mb0 p20 border text-center font-weight-bold">{variables.NO_DATA}</p>
+                <div className="p20">
+                  <Empty />
+                </div>
               )}
               {loading['overView/GET_DATA_NOTE'] ? (
                 <>
