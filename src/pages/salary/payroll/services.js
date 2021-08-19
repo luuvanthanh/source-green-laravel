81d6d5e6a8cdac749edfd1145bf55808de4cd86e
PhuKit <vanphudhsp2015@gmail.com>
2021-08-19 23:24:25 +0700

    Updated search payrolls

--- src/pages/salary/payroll/services.js
+++ src/pages/salary/payroll/services.js
@@ -1,4 +1,5 @@
 import request from '@/utils/requestLavarel';
+import { Helper, variables } from '@/utils';
 
 export function get(data = {}) {
   return request('/v1/payrolls', {
@@ -8,6 +9,14 @@ export function get(data = {}) {
       orderBy: 'CreationTime',
       sortedBy: 'desc',
       searchJoin: 'and',
+      month: Helper.getDateTime({
+        value: Helper.setDate({
+          ...variables.setDateData,
+          originValue: data.month,
+        }),
+        format: variables.DATE_FORMAT.DATE_AFTER,
+        isUTC: false,
+      }),
     },
   });
 }
