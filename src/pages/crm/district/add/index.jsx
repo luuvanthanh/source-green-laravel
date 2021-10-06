030bbc4d63282280c2760bcbbbd3d19c4252cc9d
Phạm Công Thành <phamthanht26u@gmail.com>
2021-10-06 10:48:32 +0700

    feature/API-CRM-category-citys

--- src/pages/crm/district/add/index.jsx
+++ src/pages/crm/district/add/index.jsx
@@ -1,70 +1,198 @@
 import React, { PureComponent } from 'react';
-import { connect } from 'umi';
+import { connect, history } from 'umi';
 import { Form } from 'antd';
 import styles from '@/assets/styles/Common/common.scss';
+import { isEmpty, head } from 'lodash';
+import Loading from '@/components/CommonComponent/Loading';
 import Button from '@/components/CommonComponent/Button';
-import FormItem from '@/components/CommonComponent/FormItem';
-import { variables } from '@/utils';
 import Heading from '@/components/CommonComponent/Heading';
 import Pane from '@/components/CommonComponent/Pane';
+import FormItem from '@/components/CommonComponent/FormItem';
+import { variables } from '@/utils';
 import Breadcrumbs from '@/components/LayoutComponents/Breadcrumbs';
 import PropTypes from 'prop-types';
 
-const mapStateToProps = ({ menu, crmDistrictAdd, loading }) => ({
+let isMounted = true;
+/**
+ * Set isMounted
+ * @param {boolean} value
+ * @returns {boolean} value of isMounted
+ */
+const setIsMounted = (value = true) => {
+  isMounted = value;
+  return isMounted;
+};
+/**
+ * Get isMounted
+ * @returns {boolean} value of isMounted
+ */
+const getIsMounted = () => isMounted;
+const mapStateToProps = ({ menu, loading, crmDistrictsAdd }) => ({
   loading,
-  categories: crmDistrictAdd.categories,
-  details: crmDistrictAdd.details,
-  branches: crmDistrictAdd.branches,
   menuData: menu.menuLeftCRM,
+  details: crmDistrictsAdd.details,
+  error: crmDistrictsAdd.error,
+  city: crmDistrictsAdd.city,
+  paramaterValues: crmDistrictsAdd.paramaterValues,
 });
 
 @connect(mapStateToProps)
 class Index extends PureComponent {
   formRef = React.createRef();
 
+  constructor(props, context) {
+    super(props, context);
+    this.state = {};
+    setIsMounted(true);
+  }
+
+  componentDidMount() {
+    const {
+      dispatch,
+      match: { params },
+    } = this.props;
+    dispatch({
+      type: 'crmDistrictsAdd/GET_CITY',
+      payload: params,
+    });
+    if (params.id) {
+      dispatch({
+        type: 'crmDistrictsAdd/GET_DETAILS',
+        payload: params,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const {
+      details,
+      match: { params },
+    } = this.props;
+    if (details !== prevProps.details && !isEmpty(details) && params.id) {
+      this.formRef.current.setFieldsValue({
+        ...details,
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    setIsMounted(false);
+  }
+
+  /**
+   * Set state properties
+   * @param {object} data the data input
+   * @param {function} callback the function which will be called after setState
+   * @returns {void} call this.setState to update state
+   * @memberof setStateData
+   */
+  setStateData = (state, callback) => {
+    if (!getIsMounted()) {
+      return;
+    }
+    this.setState(state, callback);
+  };
+
+  onFinish = (values) => {
+    const {
+      dispatch,
+      match: { params },
+    } = this.props;
+    const payload = {
+      ...values,
+      id: params.id,
+    };
+    dispatch({
+      type: params.id ? 'crmDistrictsAdd/UPDATE' : 'crmDistrictsAdd/ADD',
+      payload,
+      callback: (response, error) => {
+        if (response) {
+          history.goBack();
+        }
+        if (error) {
+          if (error?.validationErrors && !isEmpty(error?.validationErrors)) {
+            error?.validationErrors.forEach((item) => {
+              this.formRef.current.setFields([
+                {
+                  name: head(item.members),
+                  errors: [item.message],
+                },
+              ]);
+            });
+          }
+        }
+      },
+    });
+  };
+
   render() {
-    const { menuData, branches } = this.props;
+    const {
+      error,
+      menuData,
+      city,
+      loading: { effects },
+      match: { params },
+    } = this.props;
+    const loadingSubmit = effects['crmDistrictsAdd/ADD'] || effects['crmDistrictsAdd/UPDATE'];
+    const loading = effects['crmDistrictsAdd/GET_DETAILS'];
     return (
       <>
-        <Breadcrumbs last="Thêm mới" menu={menuData} />
-        <Pane className="col-lg-8 offset-lg-2">
-          <Form className={styles['layout-form']} layout="vertical">
-            <Pane className="pl20 pr20 mt20">
-              <Pane className="card">
-                <Pane className="p20">
-                  <Heading type="form-title" className="mb20">
-                    Thông tin thêm mới
-                  </Heading>
-                  <Pane className="row mt20">
-                    <Pane className="col-lg-12">
-                      <FormItem
-                        label="Tên quận huyện"
-                        name="district"
-                        type={variables.INPUT}
-                        rules={[variables.RULES.EMPTY]}
-                      />
-                    </Pane>
-                    <Pane className="col-lg-12">
-                      <FormItem
-                        label="Thuộc tỉnh thành"
-                        name="townward"
-                        data={branches}
-                        type={variables.SELECT}
-                        rules={[variables.RULES.EMPTY]}
-                      />
+        <Breadcrumbs last={params.id ? 'Chỉnh sửa ' : 'Tạo mới'} menu={menuData} />
+        <Pane className="col-lg-6 offset-lg-3">
+          <Form
+            className={styles['layout-form']}
+            layout="vertical"
+            colon={false}
+            ref={this.formRef}
+            onFinish={this.onFinish}
+          >
+            <Loading loading={loading} isError={error.isError} params={{ error }}>
+              <div className={styles['content-form']}>
+                <Pane className="pl20 pr20 mt20">
+                  <Pane className="card">
+                    <Pane className="p20">
+                      <Heading type="form-title" className="mb20">
+                        Thông tin thêm mới
+                      </Heading>
+                      <Pane className="row mt20">
+                        <Pane className="col-lg-12">
+                          <FormItem label="Tên quận huyện" name="name" type={variables.INPUT} />
+                        </Pane>
+                        <Pane className="col-lg-12">
+                          <FormItem
+                            options={['id', 'name']}
+                            name="city_id"
+                            data={city}
+                            placeholder="Chọn"
+                            type={variables.SELECT}
+                            label="Thuộc tỉnh thành"
+                          />
+                        </Pane>
+                      </Pane>
                     </Pane>
                   </Pane>
+                  <Pane className="p20 d-flex justify-content-between align-items-center border-top">
+                    <p
+                      className="btn-delete"
+                      role="presentation"
+                      loading={loadingSubmit}
+                      onClick={() => history.goBack()}
+                    >
+                      Hủy
+                    </p>
+                    <Button
+                      className="ml-auto px25"
+                      color="success"
+                      htmlType="submit"
+                      size="large"
+                      loading={loadingSubmit}
+                    >
+                      Lưu
+                    </Button>
+                  </Pane>
                 </Pane>
-              </Pane>
-              <Pane className="p20 d-flex justify-content-between align-items-center border-top">
-                <p className="btn-delete" role="presentation">
-                  Hủy
-                </p>
-                <Button className="ml-auto px25" color="success" htmlType="submit" size="large">
-                  Lưu
-                </Button>
-              </Pane>
-            </Pane>
+              </div>
+            </Loading>
           </Form>
         </Pane>
       </>
@@ -73,13 +201,23 @@ class Index extends PureComponent {
 }
 
 Index.propTypes = {
+  match: PropTypes.objectOf(PropTypes.any),
   menuData: PropTypes.arrayOf(PropTypes.any),
-  branches: PropTypes.arrayOf(PropTypes.any),
+  loading: PropTypes.objectOf(PropTypes.any),
+  dispatch: PropTypes.objectOf(PropTypes.any),
+  error: PropTypes.objectOf(PropTypes.any),
+  details: PropTypes.objectOf(PropTypes.any),
+  city: PropTypes.arrayOf(PropTypes.any),
 };
 
 Index.defaultProps = {
+  match: {},
   menuData: [],
-  branches: [],
+  loading: {},
+  dispatch: {},
+  error: {},
+  details: {},
+  city: [],
 };
 
 export default Index;
--- src/pages/crm/tags/model.js
+++ src/pages/crm/tags/model.js
@@ -1,39 +1,17 @@
+import * as services from './services';
+
 export default {
   namespace: 'crmTags',
   state: {
-    data: [
-      {
-        id: 1,
-        code: 'TTL01',
-        name: 'Namvv',
-        nameGroup: 'Nguyễn Văn Nam',
-      },
-      {
-        id: 2,
-        code: 'TTL01',
-        name: 'Namvv',
-        nameGroup: 'Nguyễn Văn Nam',
-      },
-    ],
-    pagination: {
-      total: 0,
-    },
+    details: {},
+    tags: [],
     error: {
       isError: false,
       data: {},
     },
   },
   reducers: {
-    INIT_STATE: (state) => ({
-      ...state,
-      isError: false,
-      data: [],
-    }),
-    SET_DATA: (state, { payload }) => ({
-      ...state,
-      data: payload.parsePayload,
-      pagination: payload.pagination,
-    }),
+    INIT_STATE: (state) => ({ ...state, isError: false, data: [] }),
     SET_ERROR: (state, { payload }) => ({
       ...state,
       error: {
@@ -43,7 +21,48 @@ export default {
         },
       },
     }),
+    SET_DETAILS: (state, { payload }) => ({
+      ...state,
+      details: payload,
+    }),
+    SET_TAGS: (state, { payload }) => ({
+      ...state,
+      tags: payload,
+    }),
   },
-  effects: {},
+  effects: {
+    *GET_TAGS({ payload }, saga) {
+      try {
+        const response = yield saga.call(services.getTags, payload);
+        if (response) {
+          yield saga.put({
+            type: 'SET_TAGS',
+            payload: response.parsePayload,
+          });
+        }
+      } catch (error) {
+        yield saga.put({
+          type: 'SET_ERROR',
+          payload: error.data,
+        });
+      }
+    },
+    *ADD({ payload, callback }, saga) {
+      try {
+        yield saga.call(services.add, payload);
+        callback(payload);
+      } catch (error) {
+        callback(null, error);
+      }
+    },
+    *REMOVE({ payload, callback }, saga) {
+      try {
+        yield saga.call(services.remove, payload.id);
+        callback(payload);
+      } catch (error) {
+        callback(null, error);
+      }
+    },
+  },
   subscriptions: {},
 };
