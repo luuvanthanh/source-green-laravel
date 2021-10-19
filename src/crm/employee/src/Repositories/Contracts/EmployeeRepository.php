b0bac56af29ccc8db44e8b54795b41b1ff249ecf
tamnguyen <nhattam253@gmail.com>
2021-10-19 11:32:59 +0700

    feat(Employee): crud employee

---
+++ src/crm/employee/src/Repositories/Contracts/EmployeeRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GGPHP\Crm\Employee\Repositories\Contracts;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface CustomerPotentialEventInfoRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface EmployeeRepository extends RepositoryInterface
+{
+    //
+}
