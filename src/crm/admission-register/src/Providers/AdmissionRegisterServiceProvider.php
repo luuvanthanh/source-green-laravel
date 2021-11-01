b36c8bbbe79cd7dfd1d3d6b3ff5325b36e4a4b91
dangnguyenvan <dangnguyenvan.info@gmail.com>
2021-11-01 15:16:43 +0700

    feat(AdmissionRegister): manage confirm transporters List confirm transporters Add confirm transporters

--- src/crm/admission-register/src/Providers/AdmissionRegisterServiceProvider.php
+++ src/crm/admission-register/src/Providers/AdmissionRegisterServiceProvider.php
@@ -3,7 +3,9 @@
 namespace GGPHP\Crm\AdmissionRegister\Providers;
 
 use GGPHP\Crm\AdmissionRegister\Repositories\Contracts\AdmissionRegisterRepository;
+use GGPHP\Crm\AdmissionRegister\Repositories\Contracts\ConfirmTransporterRepository;
 use GGPHP\Crm\AdmissionRegister\Repositories\Eloquent\AdmissionRegisterRepositoryEloquent;
+use GGPHP\Crm\AdmissionRegister\Repositories\Eloquent\ConfirmTransporterRepositoryEloquent;
 use Illuminate\Support\ServiceProvider;
 
 class AdmissionRegisterServiceProvider extends ServiceProvider
@@ -28,5 +30,6 @@ class AdmissionRegisterServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(AdmissionRegisterRepository::class, AdmissionRegisterRepositoryEloquent::class);
+        $this->app->bind(ConfirmTransporterRepository::class, ConfirmTransporterRepositoryEloquent::class);
     }
 }
