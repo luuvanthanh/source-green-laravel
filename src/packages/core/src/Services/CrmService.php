43b19b3bc78db20a695528234914347df109c542
dangnguyenvan <dangnguyenvan.info@gmail.com>
2021-11-02 14:36:29 +0700

    feat(Category): update branch Add event create branch crm Add event update branch crm Add event delete branch crm

--- src/packages/core/src/Services/CrmService.php
+++ src/packages/core/src/Services/CrmService.php
@@ -23,4 +23,54 @@ class CrmService
 
         return json_decode($response->body());
     }
+
+    public static function createBranch(array $attributes)
+    {
+        $url = env('CRM_URL') . '/api/v1/branches';
+
+        $response = Http::post($url, $attributes);
+
+        if ($response->failed()) {
+            $message = "Có lỗi từ api CRM";
+            if (isset(json_decode($response->body())->error) && isset(json_decode($response->body())->error->message)) {
+                $message = "CRM: " . json_decode($response->body())->error->message;
+            }
+            throw new HttpException(500, $message);
+        }
+
+        return json_decode($response->body());
+    }
+
+    public static function updateBranch(array $attributes, $id)
+    {
+        $url = env('CRM_URL') . '/api/v1/branches/' . $id;
+
+        $response = Http::put($url, $attributes);
+
+        if ($response->failed()) {
+            $message = "Có lỗi từ api CRM";
+            if (isset(json_decode($response->body())->error) && isset(json_decode($response->body())->error->message)) {
+                $message = "CRM: " . json_decode($response->body())->error->message;
+            }
+            throw new HttpException(500, $message);
+        }
+
+        return json_decode($response->body());
+    }
+    public static function deleteBranch($id)
+    {
+        $url = env('CRM_URL') . '/api/v1/branches/' . $id;
+
+        $response = Http::delete($url);
+
+        if ($response->failed()) {
+            $message = "Có lỗi từ api CRM";
+            if (isset(json_decode($response->body())->error) && isset(json_decode($response->body())->error->message)) {
+                $message = "CRM: " . json_decode($response->body())->error->message;
+            }
+            throw new HttpException(500, $message);
+        }
+
+        return json_decode($response->body());
+    }
 }
