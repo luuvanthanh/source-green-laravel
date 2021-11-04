e3123304ce9c9b8b04fe8d9fbe805b7265f52f27
Phạm Công Thành <phamthanht26u@gmail.com>
2021-11-04 11:39:09 +0700

    feature/API-CRM-SALE-admission-registers

--- src/pages/crm/sale-admission-registers/create/model.js
+++ src/pages/crm/sale-admission-registers/create/model.js
@@ -1,91 +1,110 @@
-export default {
-  namespace: 'crmSaleAdmissionAdd',
-  state: {
-    details: {},
-    data: [
-      {
-        id: 1,
-        key: 1,
-        full_name: 'Nguyễn văn A',
-        birth_day : '20/10/2019',
-        age: 20,
-        time: '20/10/2019',
-        name_parents: 'Nguyễn văn Đạt',
-        name_mon : 'Nguyễn Thị bé',
-        status: 'Đăng ký mới',
-      },
-      {
-        id: 2,
-        key: 2,
-        full_name: 'Nguyễn văn A',
-        birth_day : '20/10/2019',
-        age: 20,
-        time: '20/10/2019',
-        name_parents: 'Nguyễn văn Đạt',
-        name_mon : 'Nguyễn Thị bé',
-        status: 'Đăng ký mới',
-      },
-      {
-        id: 3,
-        key: 3,
-        full_name: 'Nguyễn văn A',
-        birth_day : '20/10/2019',
-        age: 20,
-        time: '20/10/2019',
-        name_parents: 'Nguyễn văn Đạt',
-        name_mon : 'Nguyễn Thị bé',
-        status: 'Đăng ký mới',
-      },
-      {
-        id: 4,
-        key: 4,
-        full_name: 'Nguyễn văn A',
-        birth_day : '20/10/2019',
-        age: 20,
-        time: '20/10/2019',
-        name_parents: 'Nguyễn văn Đạt',
-        name_mon : 'Nguyễn Thị bé',
-        status: 'Đăng ký mới',
-      }
-    ],
-    detailsLead: {},
-    detailsTags: [],
-    detailsReferences: [],
-    error: {
-      isError: false,
-      data: {},
-    },
-    classTypes: [],
-    sensitivePeriods: [],
-    parents: [],
-    employees: [],
-    branches: [],
-    classes: [],
-    city: [],
-    district: [],
-    student: [],
-    lead: [],
-    parentLead: [],
-  },
-  reducers: {
-    INIT_STATE: (state) => ({
-      ...state,
-      detailsLead: {},
-      isError: false,
-      data: [],
-    }),
-    SET_DATA: (state, { payload }) => ({
-      ...state,
-      details: payload,
-      detailsTags: payload,
-    }),
-    SET_DETAILS: (state, { payload }) => ({
-      ...state,
-      details: payload.parsePayload,
+import * as services from './services';
+
+  export default {
+    namespace: 'crmSaleAdmissionAdd',
+    state: {
+      details: {},
+      customerLead: [],
+      studentsLead: [],
       error: {
-        status: null,
         isError: false,
+        data: {},
       },
-    }),
-  },
-};
+    },
+    reducers: {
+      INIT_STATE: (state) => ({ ...state, isError: false, data: [] }),
+      SET_ERROR: (state, { payload }) => ({
+        ...state,
+        error: {
+          isError: true,
+          data: {
+            ...payload,
+          },
+        },
+      }),
+      SET_DETAILS: (state, { payload }) => ({
+        ...state,
+        details: payload,
+      }),
+      SET_CUSTOMER_LEAD: (state, { payload }) => ({
+        ...state,
+        customerLead: payload.parsePayload,
+      }),
+      SET_STUDENTS_LEAD: (state, { payload }) => ({
+        ...state,
+        studentsLead: payload.parsePayload,
+      }),
+    },
+    effects: {
+      *GET_DETAILS({ payload }, saga) {
+        try {
+          const response = yield saga.call(services.details, payload);
+          if (response) {
+            yield saga.put({
+              type: 'SET_DETAILS',
+              payload: response.parsePayload,
+            });
+          }
+        } catch (error) {
+          yield saga.put({
+            type: 'SET_ERROR',
+            payload: error.data,
+          });
+        }
+      },
+      *ADD({ payload, callback }, saga) {
+        try {
+          yield saga.call(services.add, payload);
+          callback(payload);
+        } catch (error) {
+          callback(null, error);
+        }
+      },
+      *UPDATE({ payload, callback }, saga) {
+        try {
+          yield saga.call(services.update, payload);
+          callback(payload);
+        } catch (error) {
+          callback(null, error);
+        }
+      },
+      *REMOVE({ payload, callback }, saga) {
+        try {
+          yield saga.call(services.remove, payload.id);
+          callback(payload);
+        } catch (error) {
+          callback(null, error);
+        }
+      },
+      *GET_CUSTOMER_LEAD({ payload }, saga) {
+        try {
+          const response = yield saga.call(services.getCustomerLead, payload);
+          yield saga.put({
+            type: 'SET_CUSTOMER_LEAD',
+            payload: response,
+          });
+        } catch (error) {
+          yield saga.put({
+            type: 'SET_ERROR',
+            payload: error.data,
+          });
+        }
+      },
+      *GET_STUDENTS_LEAD({ payload ,callback}, saga) {
+        try {
+          const response = yield saga.call(services.getStudentsLead, payload);
+          callback(response);
+          yield saga.put({
+            type: 'SET_STUDENTS_LEAD',
+            payload: response,
+          });
+        } catch (error) {
+          yield saga.put({
+            type: 'SET_ERROR',
+            payload: error.data,
+          });
+        }
+      },
+    },
+    subscriptions: {},
+  };
---
+++ src/pages/crm/sale-admission-registers/create/services.js
@@ -0,0 +1,38 @@
+import request from '@/utils/requestCrm';
+import { Helper, variables } from '@/utils';
+
+export function add(data = {}) {
+  return request('/v1/admission-registers', {
+    method: 'POST',
+    data : {
+      ...data,
+      date_register: Helper.getDateTime({
+        value: Helper.setDate({
+          ...variables.setDateData,
+          originValue: data.date_register,
+        }),
+        format: variables.DATE_FORMAT.DATE_AFTER,
+        isUTC: false,
+      }),
+    }
+  });
+}
+
+export function getCustomerLead() {
+  return request(`/v1/customer-leads`, {
+    method: 'GET',
+    params: {
+      orderBy: 'full_name',
+    },
+  });
+}
+
+  export function getStudentsLead(params) {
+    return request(`/v1/student-infos`, {
+      method: 'GET',
+      params: {
+        ...params,
+        orderBy: 'full_name',
+      },
+    });
+  }
