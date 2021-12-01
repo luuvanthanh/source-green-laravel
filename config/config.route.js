3cd60d97ed395f2a27026da1ee44d91f493e326b
Thanh <phong@PhuTV.local>
2021-12-01 16:26:39 +0700

    feature/UI-CRM-CATEGORY-skill

--- config/config.route.js
+++ config/config.route.js
@@ -2419,6 +2419,27 @@ export default [
             wrappers: ['@/wrappers/auth'],
             authority: [permissions.CRM],
           },
+
+          // chilldrensProblems
+          {
+            path: '/crm/danh-muc/van-de-kho-khan-cua-tre',
+            component: './crm/childrens-problems',
+            wrappers: ['@/wrappers/auth'],
+            authority: [permissions.CRM],
+          },
+          {
+            path: '/crm/danh-muc/van-de-kho-khan-cua-tre/tao-moi',
+            component: './crm/childrens-problems/add',
+            wrappers: ['@/wrappers/auth'],
+            authority: [permissions.CRM],
+          },
+          {
+            path: '/crm/danh-muc/van-de-kho-khan-cua-tre/:id/chi-tiet',
+            component: './crm/childrens-problems/add',
+            wrappers: ['@/wrappers/auth'],
+            authority: [permissions.CRM],
+          },
+          //chilldrensProblems
           {
             path: '/crm/sale/ph-lead',
             component: './crm/sale-parents-lead',
