1c92536295f5eac6f5dffbb00a75752160d7137a
Thanh <phong@PhuTV.local>
2021-12-02 14:42:57 +0700

    feature/UI-CRM-CATEGORY-reviewScenario

---
+++ src/pages/crm/evaluate-parents/add/model.js
@@ -0,0 +1,54 @@
+import * as services from './services';
+
+  export default {
+    namespace: 'crmEvaluateParentsAdd',
+    state: {
+      error: {
+        isError: false,
+        data: {},
+      },
+      details: {},
+    },
+    reducers: {
+      INIT_STATE: (state) => ({ ...state, isError: false, data: [] }),
+      SET_ERROR: (state, { payload }) => ({
+        ...state,
+        error: {
+          isError: true,
+          data: {
+            ...payload,
+          },
+        },
+      }),
+      SET_DETAILS: (state, { payload }) => ({
+        ...state,
+        details: payload,
+      }),
+    },
+    effects: {
+      *GET_DETAILS({ payload }, saga) {
+        try {
+          const response = yield saga.call(services.get, payload);
+          yield saga.put({
+            type: 'SET_DETAILS',
+            payload: response,
+          });
+        } catch (error) {
+          yield saga.put({
+            type: 'SET_ERROR',
+            payload: error.data,
+          });
+        }
+      },
+      *REMOVE({ payload, callback }, saga) {
+        try {
+          yield saga.call(services.remove, payload);
+          callback(payload);
+        } catch (error) {
+          callback(null, error);
+        }
+      },
+    },
+    subscriptions: {},
+  };
+  
