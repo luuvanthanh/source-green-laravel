9cb08ef6451f66e91bc25e6d460e5a6922323f61
DinhThuongNguyen <dinhthuong092@gmail.com>
2021-12-29 19:56:15 +0700

    append api in table

--- src/components/CommonComponent/CardCalendar/CardMonth/index.jsx
+++ src/components/CommonComponent/CardCalendar/CardMonth/index.jsx
@@ -1,19 +1,23 @@
 import { Helper, variables } from '@/utils';
 import classNames from 'classnames';
 import Text from '@/components/CommonComponent/Text';
-import moment from 'moment';
 import PropType from 'prop-types';
 import { Button, List } from 'antd';
 import { useState } from 'react';
 import styles from '../style.module.scss';
 
-const CardMonth = ({ date, month, data, onClick }) => {
-  const start = moment(date).add(7, 'hours').add(30, 'minutes');
-  const startModal = moment(date).add(7, 'hours').add(30, 'minutes');
+const CardMonth = ({ date, month, data, handleClick }) => {
   const [show, setShow] = useState(false);
+  const clickButton = (value, type) => {
+    handleClick(value, type);
+  };
+
   return (
     <div
-      className={classNames('w-100 d-flex flex-column justify-content-between', styles['card-month'])}
+      className={classNames(
+        'w-100 d-flex flex-column justify-content-between',
+        styles['card-month'],
+      )}
     >
       {Helper.getDate(date, variables.DATE_FORMAT.MONTH) ===
       Helper.getDate(month, variables.DATE_FORMAT.MONTH) ? (
@@ -27,68 +31,58 @@ const CardMonth = ({ date, month, data, onClick }) => {
       )}
 
       <div className="d-flex flex-column">
-        <Button
-          className={classNames('w-100 d-flex flex-row align-items-center', styles.rmPadding)}
-          onClick={onClick}
-        >
-          <span className={styles.dotStyle} />
-          <Text className="p-1" color="dark" size="normal">
-            {Helper.getDate(start.add(0, 'minute').clone(), variables.DATE_FORMAT.HOUR)}
-          </Text>
-          <span>-</span>
-          <Text className="p-1" color="dark" size="normal">
-            {Helper.getDate(
-              start.add(data[0].timeLearn, 'minutes').clone(),
-              variables.DATE_FORMAT.HOUR,
-            )}
-          </Text>
-          <span>:</span>
-          <Text className="p-1" color="dark" size="normal">
-            {data[0].title}
-          </Text>
-        </Button>
-        <Button
-          className={classNames('w-100 d-flex flex-row align-items-center', styles.rmPadding)}
-          onClick={onClick}
-        >
-          <span className={styles.dotStyle} />
-          <Text className="p-1" color="dark" size="normal">
-            {Helper.getDate(start.add(0, 'minute').clone(), variables.DATE_FORMAT.HOUR)}
-          </Text>
-          <span> - </span>
-          <Text className="p-1" color="dark" size="normal">
-            {Helper.getDate(
-              start.add(data[1].timeLearn, 'minutes').clone(),
-              variables.DATE_FORMAT.HOUR,
-            )}
-          </Text>
-          <span>:</span>
-          <Text className="p-1" color="dark" size="normal">
-            {data[1].title}
-          </Text>
-        </Button>
-        <Button
-          className={classNames('w-100 d-flex flex-row align-items-center', styles.rmPadding)}
-          onClick={() => setShow(true)}
-        >{`+ ${data.length} hoạt động khác`}</Button>
+        {data?.map(
+          (item, idx) =>
+            idx < 2 && (
+              <Button
+                className={classNames('w-100 d-flex flex-row align-items-center', styles.rmPadding)}
+                onClick={() => clickButton(item, 'dayGridMonth')}
+                key={idx}
+              >
+                <span className={styles.dotStyle} />
+                <Text className="p-1" color="dark" size="normal">
+                  {item.startTime}
+                </Text>
+                <span> - </span>
+                <Text className="p-1" color="dark" size="normal">
+                  {item.endTime}
+                </Text>
+                <span>:</span>
+                <Text className="p-1" color="dark" size="normal">
+                  {item?.class?.name}
+                </Text>
+              </Button>
+            ),
+        )}
+        {data?.length > 2 && (
+          <Button
+            className={classNames('w-100 d-flex flex-row align-items-center', styles.rmPadding)}
+            onClick={() => setShow(true)}
+          >{`+ ${data?.length - 2} hoạt động khác`}</Button>
+        )}
       </div>
-      <div className={styles.list} style={{display: show ? 'block' : 'none'}}>
+      <div className={styles.list} style={{ display: show ? 'block' : 'none' }}>
         <List
           bordered
           dataSource={data}
           renderItem={(item) => (
-              <List.Item>
-              <Button className='w-100 d-flex align-items-center justify-content-left' >
+            <List.Item>
+              <Button
+                className="w-100 d-flex align-items-center justify-content-left"
+                onClick={() => clickButton(item, 'dayGridMonth')}
+              >
                 <span className={classNames('mr-2', styles.dotStyle)} />
-                <span>{Helper.getDate(startModal.add(0, 'minute').clone(), variables.DATE_FORMAT.HOUR)}</span>
-                <span className='pl-2 pr-2'>-</span>
-                <span>{Helper.getDate(startModal.add(item.timeLearn, 'minutes').clone(), variables.DATE_FORMAT.HOUR)}</span>
-                <span className='pl-3'>{item.title}</span>
+                <span>{item.startTime}</span>
+                <span className="pl-2 pr-2">-</span>
+                <span>{item.endTime}</span>
+                <span className="pl-3">{item?.class?.name}</span>
               </Button>
             </List.Item>
-            )}
+          )}
         />
-        <Button className={styles.btnClose} onClick={() => setShow(false)}>X</Button>
+        <Button className={styles.btnClose} onClick={() => setShow(false)}>
+          X
+        </Button>
       </div>
     </div>
   );
@@ -97,15 +91,15 @@ const CardMonth = ({ date, month, data, onClick }) => {
 export default CardMonth;
 
 CardMonth.propTypes = {
-  date: PropType.any,
+  date: PropType.objectOf(PropType.any),
   month: PropType.any,
-  onClick: PropType.func,
+  handleClick: PropType.func,
   data: PropType.arrayOf(PropType.any),
 };
 
 CardMonth.defaultProps = {
-  date: '',
+  date: {},
   month: '',
-  onClick: () => {},
+  handleClick: () => {},
   data: [],
 };
