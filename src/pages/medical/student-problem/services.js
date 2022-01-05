feeb75c82387ca3f2489604f3ecbf59840f9c55e
Thanh <phong@PhuTV.local>
2022-01-05 10:12:26 +0700

    fix/API-MEDICAL

--- src/pages/medical/student-problem/services.js
+++ src/pages/medical/student-problem/services.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request';
 import { omit } from 'lodash';
-import { Helper } from '@/utils';
+import { Helper, variables } from '@/utils';
 
 export function get(params = {}) {
   return request('/student-medical-problems', {
@@ -8,6 +8,24 @@ export function get(params = {}) {
     params: {
       ...omit(params, 'page', 'limit', 'date'),
       ...Helper.getPagination(params.page, params.limit),
+      FromDate: Helper.getDateTime({
+        value: Helper.setDate({
+          ...variables.setDateData,
+          originValue: params.date,
+          targetValue: '23:59:59',
+        }),
+        format: variables.DATE_FORMAT.DATE_AFTER,
+        isUTC: true,
+      }),
+      ToDate: Helper.getDateTime({
+        value: Helper.setDate({
+          ...variables.setDateData,
+          originValue: params.date,
+          targetValue: '23:59:59',
+        }),
+        format: variables.DATE_FORMAT.DATE_AFTER,
+        isUTC: true,
+      }),
     },
   });
 }
