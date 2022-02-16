24df88b17c5b7b8730d75f169614de086365923f
Thanh <phong@PhuTV.local>
2022-02-16 10:37:29 +0700

    feature/api-hrm-reportOnLeave

---
+++ src/pages/hrm/report/employee-on-leave/services.js
@@ -0,0 +1,33 @@
+import request from '@/utils/requestLavarel';
+import { omit } from 'lodash';
+import { Helper, variables } from '@/utils';
+
+export function get(params = {}) {
+  return request('/v1/report-absents', {
+    method: 'GET',
+    params: {
+      ...omit(params, 'page', 'limit'),
+      ...Helper.getPagination(params.page, params.limit),
+    },
+  });
+}
+
+export function getDivisions(_params = {}) {
+  return request('/v1/divisions', {
+    method: 'GET',
+    params: {
+      limit: variables.PAGINATION.SIZEMAX,
+      page: variables.PAGINATION.PAGE,
+    },
+  });
+}
+
+export function getPositions(_params = {}) {
+  return request('/v1/positions', {
+    method: 'GET',
+    params: {
+      limit: variables.PAGINATION.SIZEMAX,
+      page: variables.PAGINATION.PAGE,
+    },
+  });
+}
