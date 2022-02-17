ec5cbcc6821a34ea493a1c35229a9f5be1f750d5
Thanh <phong@PhuTV.local>
2022-02-17 17:18:03 +0700

    feature/api-hrm-report

--- src/pages/hrm/report/working-seniority/services.js
+++ src/pages/hrm/report/working-seniority/services.js
@@ -1,20 +1,36 @@
-import request from '@/utils/request';
+import request from '@/utils/requestLavarel';
 import { omit } from 'lodash';
-import { Helper } from '@/utils';
+import { Helper, variables } from '@/utils';
 
 export function get(params = {}) {
-  return request('/student-medical-problems', {
+  return request('/v1/labours-contracts', {
     method: 'GET',
     params: {
-      ...omit(params, 'page', 'limit', 'date'),
+      ...omit(params, 'page', 'limit'),
       ...Helper.getPagination(params.page, params.limit),
+      include: Helper.convertIncludes([
+       'employee,typeOfContract,position,branch'
+      ]),
     },
   });
 }
 
-export function remove(id = {}) {
-  return request(`/student-medical-problems/${id}`, {
-    method: 'DELETE',
-    parse: true,
+export function getDivisions(_params = {}) {
+  return request('/v1/divisions', {
+    method: 'GET',
+    params: {
+      limit: variables.PAGINATION.SIZEMAX,
+      page: variables.PAGINATION.PAGE,
+    },
+  });
+}
+
+export function getPositions(_params = {}) {
+  return request('/v1/positions', {
+    method: 'GET',
+    params: {
+      limit: variables.PAGINATION.SIZEMAX,
+      page: variables.PAGINATION.PAGE,
+    },
   });
 }
