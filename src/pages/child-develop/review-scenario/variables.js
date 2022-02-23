ab824d411a7f28658e70f2150f28affed113c580
Thanh <phong@PhuTV.local>
2022-02-23 17:50:00 +0700

    update/api-crm-facebook

--- src/pages/child-develop/review-scenario/variables.js
+++ src/pages/child-develop/review-scenario/variables.js
@@ -10,6 +10,17 @@ export const variables = {
     { id: '7', name: '36 - 50 tháng' },
     { id: '8', name: '50- 60 tháng' },
   ],
+  AGE_TABLE: [
+    { id: '0-6', name: '0 - 6 tháng' }, 
+    { id: '6-9', name: '6 - 9 tháng' },
+    { id: '9-12', name: '9 - 12 tháng' },
+    { id: '12-18', name: '12 - 18 tháng' },
+    { id: '18-24', name: '18 - 24 tháng' },
+    { id: '24-30', name: '24 - 30 tháng' },
+    { id: '30-36', name: '30 - 36 tháng' },
+    { id: '36-50', name: '36 - 50 tháng' },
+    { id: '50-60', name: '50- 60 tháng' },
+  ],
   APPLY: [
     { id: 'inputAssessment', name: 'Test đầu vào' },
     { id: 'periodicAssessment', name: 'Đánh giá định kỳ' },
