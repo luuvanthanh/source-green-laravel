af5da2d8cdcc275c7a7f1850e77e9ad9e776ddd5
tamnn <nhattam253@gmail.com>
2022-05-19 14:29:13 +0700

    feat(TeacherAssignment): manage teacher assignment

---
+++ src/hrm/teacher-assignment/src/Repositories/Contracts/TeacherAssignmentRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GGPHP\TeacherAssignment\Repositories\Contracts;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface TeacherAssignmentRepository.
+ *
+ * @package namespace App\Repositories\Contracts;
+ */
+interface TeacherAssignmentRepository extends RepositoryInterface
+{
+}
