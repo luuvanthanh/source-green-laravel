d1f268bb01e48a84c408b9cb28c6d207afe50acc
ThanhPC <thanhpc@greenglobal.vn>
2022-06-23 11:44:25 +0700

    feature api fees

--- config/config.route.js
+++ config/config.route.js
@@ -2851,7 +2851,7 @@ export default [
             path: '/crm/chat/facebook',
             component: './crm/messages/facebook-dev-v1',
             wrappers: ['@/wrappers/auth'],
-            authority: [],
+            authority: [permissions.CRM],
           },
           {
             path: '/crm/chat/zalo',
