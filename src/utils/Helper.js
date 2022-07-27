22e79d647c7fc27f19be62eb1155a468ad98568e
ThanhPC <thanhpc@greenglobal.vn>
2022-07-27 16:18:51 +0700

    Fix Ui medical upload

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -1252,6 +1252,9 @@ export default class Helpers {
       moment(contractTo).format(variables.DATE_FORMAT.DATE_BEFORE),
     ).diff(moment().format(variables.DATE_FORMAT.DATE_BEFORE), 'days');
     const diffExpirationDateMonth = moment(contractTo).diff(moment(), 'month');
+    if (record?.isEffect === false) {
+      return <Tag color="danger">Đã hết hạn</Tag>;
+    }
     if (diffSignDate <= 0 && diffExpirationDateMonth > 0) {
       return <Tag color="success">Đang hiệu lực</Tag>;
     }
@@ -1265,9 +1268,6 @@ export default class Helpers {
           Gần hết hạn
         </Tag>
       );
-    }
-    if (record?.isEffect === false) {
-      return <Tag color="danger">Đã hết hạn</Tag>;
     }
     if (diffExpirationDate < 0) {
       return <Tag color="danger">Đã hết hạn</Tag>;
