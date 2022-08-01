ec0248b591d075a76d9ef90faf669ace5e308b31
Van Anh <anhcdcn98@gmail.com>
2022-08-01 11:13:02 +0700

    fix(Contract): update end date schedule

--- src/packages/profile/src/Repositories/Eloquent/LabourContractRepositoryEloquent.php
+++ src/packages/profile/src/Repositories/Eloquent/LabourContractRepositoryEloquent.php
@@ -217,11 +217,14 @@ class LabourContractRepositoryEloquent extends CoreRepositoryEloquent implements
             $divisionShift = \GGPHP\ShiftSchedule\Models\DivisionShift::where('DivisionId', $attributes['divisionId'])->where([['StartDate', '<=', $labourContract->ContractFrom->format('Y-m-d')], ['EndDate', '>=', $labourContract->ContractFrom->format('Y-m-d')]])->first();
 
             if (!is_null($divisionShift)) {
+                
+                $endDate = $labourContract->ContractTo ? $labourContract->ContractTo->format('Y-m-d') : $divisionShift->EndDate->format('Y-m-d');
+
                 $dataSchedule = [
                     'employeeId' => $attributes['employeeId'],
                     'shiftId' => $divisionShift->ShiftId,
                     'startDate' => $labourContract->ContractFrom->format('Y-m-d'),
-                    'endDate' => $labourContract->ContractTo->format('Y-m-d'),
+                    'endDate' => $endDate,
                     'interval' => 1,
                     'repeatBy' => 'daily',
                 ];
