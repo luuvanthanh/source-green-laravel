6a4cd594f94d6e512a0aa57b6e3ed33871d87ff0
ThanhPC <thanhpc@greenglobal.vn>
2022-09-05 17:06:55 +0700

    Update api shool year note

--- src/pages/fee-policy/school-year/services.js
+++ src/pages/fee-policy/school-year/services.js
@@ -4,7 +4,15 @@ export function get(params = {}) {
   return request('/v1/school-years', {
     method: 'GET',
     params: {
-      ...params
+      ...params,
     },
+  });
+}
+
+export function update(data = {}) {
+  return request(`/v1/school-years/${data.id}`, {
+    method: 'PUT',
+    data,
+    parse: true,
   });
 }
