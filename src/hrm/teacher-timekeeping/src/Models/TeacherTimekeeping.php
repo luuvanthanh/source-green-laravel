17e8f7625807d25343bc8bb3ec0d133827c8f453
tamnn <nhattam253@gmail.com>
2022-10-03 17:08:25 +0700

    Feat(TeacherTimekeeping): new Update

--- src/hrm/teacher-timekeeping/src/Models/TeacherTimekeeping.php
+++ src/hrm/teacher-timekeeping/src/Models/TeacherTimekeeping.php
@@ -2,6 +2,10 @@
 
 namespace GGPHP\TeacherTimekeeping\Models;
 
+use GGPHP\Category\Models\Branch;
+use GGPHP\Clover\Models\Classes;
+use GGPHP\Clover\Models\ClassProjectSession;
+use GGPHP\Clover\Models\Item;
 use GGPHP\Core\Models\UuidModel;
 use GGPHP\FingerprintTimekeeper\Models\FingerprintTimekeeper;
 use GGPHP\Users\Models\User;
@@ -25,13 +29,20 @@ class TeacherTimekeeping extends UuidModel
         'PHONE' => 4
     ];
 
+    const ACCOUNTANT_TYPE = [
+        'PROGRAM' => 'PROGRAM',
+        'MODULE' => 'MODULE',
+        'PROJECT' => 'PROJECT'
+    ];
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'EmployeeId', 'Status', 'Type', 'AttendedAt', 'StartTime', 'EndTime', 'TotalHourWorked', 'OverTimeHour'
+        'EmployeeId', 'Status', 'Type', 'AttendedAt', 'StartTime', 'EndTime', 'TotalHourWorked', 'OverTimeHour',
+        'BranchId', 'ClassId', 'ClassProjectSessionId', 'ProductId', 'ModuleId', 'ProjectId'
     ];
 
     protected $dateTimeFields = [
@@ -54,5 +65,35 @@ class TeacherTimekeeping extends UuidModel
     public function employee()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Item::class, 'ProductId', 'Id')->where('Type', self::ACCOUNTANT_TYPE['PROGRAM']);
+    }
+
+    public function module()
+    {
+        return $this->belongsTo(Item::class, 'ModuleId', 'Id')->where('Type', self::ACCOUNTANT_TYPE['MODULE']);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Item::class, 'ProjectId', 'Id')->where('Type', self::ACCOUNTANT_TYPE['PROJECT']);
+    }
+
+    public function classes()
+    {
+        return $this->belongsTo(Classes::class, 'ClassId', 'Id');
+    }
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class, 'BranchId', 'Id');
+    }
+
+    public function classProjectSession()
+    {
+        return $this->belongsTo(ClassProjectSession::class, 'ClassProjectSessionId', 'Id');
     }
 }
--- src/hrm/teacher-timekeeping/src/Transformers/TeacherTimekeepingTransformer.php
+++ src/hrm/teacher-timekeeping/src/Transformers/TeacherTimekeepingTransformer.php
@@ -2,6 +2,13 @@
 
 namespace GGPHP\TeacherTimekeeping\Transformers;
 
+use Carbon\Carbon;
+use GGPHP\Category\Transformers\BranchTransformer;
+use GGPHP\Clover\Transformers\ClassesTransformer;
+use GGPHP\Clover\Transformers\ClassProjectSessionTransformer;
+use GGPHP\Clover\Transformers\ModuleTransformer;
+use GGPHP\Clover\Transformers\ProductTransformer;
+use GGPHP\Clover\Transformers\ProjectTransformer;
 use GGPHP\Core\Transformers\BaseTransformer;
 use GGPHP\TeacherTimekeeping\Models\TeacherTimekeeping;
 
@@ -12,7 +19,7 @@ use GGPHP\TeacherTimekeeping\Models\TeacherTimekeeping;
  */
 class TeacherTimekeepingTransformer extends BaseTransformer
 {
-    protected $availableIncludes = [];
+    protected $availableIncludes = ['branch', 'classes', 'classProjectSession', 'product', 'module', 'project'];
     protected $defaultIncludes = [];
 
     /**
@@ -26,7 +33,62 @@ class TeacherTimekeepingTransformer extends BaseTransformer
     {
         return [
             'Status' => array_search($model->Status, TeacherTimekeeping::STATUS),
-            'Type' => array_search($model->Type, TeacherTimekeeping::TYPE)
+            'Type' => array_search($model->Type, TeacherTimekeeping::TYPE),
+            'time' => Carbon::parse($model->StartTime)->diffInMinutes(Carbon::parse($model->EndTime))
         ];
+    }
+
+    public function includeBranch(TeacherTimekeeping $teacherTimekeeping)
+    {
+        if (is_null($teacherTimekeeping->branch)) {
+            return null;
+        }
+
+        return $this->item($teacherTimekeeping->branch, new BranchTransformer, 'Branch');
+    }
+
+    public function includeClasses(TeacherTimekeeping $teacherTimekeeping)
+    {
+        if (is_null($teacherTimekeeping->classes)) {
+            return null;
+        }
+
+        return $this->item($teacherTimekeeping->classes, new ClassesTransformer, 'Classes');
+    }
+
+    public function includeClassProjectSession(TeacherTimekeeping $teacherTimekeeping)
+    {
+        if (is_null($teacherTimekeeping->classProjectSession)) {
+            return null;
+        }
+
+        return $this->item($teacherTimekeeping->classProjectSession, new ClassProjectSessionTransformer, 'ClassProjectSession');
+    }
+
+    public function includeProduct(TeacherTimekeeping $teacherTimekeeping)
+    {
+        if (is_null($teacherTimekeeping->product)) {
+            return null;
+        }
+
+        return $this->item($teacherTimekeeping->product, new ProductTransformer, 'Product');
+    }
+
+    public function includeModule(TeacherTimekeeping $teacherTimekeeping)
+    {
+        if (is_null($teacherTimekeeping->module)) {
+            return null;
+        }
+
+        return $this->item($teacherTimekeeping->module, new ModuleTransformer, 'Module');
+    }
+
+    public function includeProject(TeacherTimekeeping $teacherTimekeeping)
+    {
+        if (is_null($teacherTimekeeping->project)) {
+            return null;
+        }
+
+        return $this->item($teacherTimekeeping->project, new ProjectTransformer, 'Project');
     }
 }
