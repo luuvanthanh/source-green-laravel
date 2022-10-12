39b9d567ea3753a5aa911dcce0d0afa91bbbfe09
ThanhPC <thanhpc@greenglobal.vn>
2022-10-12 10:09:56 +0700

    Fix api child develop select age

--- src/pages/child-develop/review-scenario/variables.js
+++ src/pages/child-develop/review-scenario/variables.js
@@ -1,6 +1,6 @@
 export const variables = {
   AGE: [
-    { id: '0', name: '0 - 6 tháng' }, 
+    { id: '0', name: '0 - 6 tháng' },
     { id: '1', name: '6 - 9 tháng' },
     { id: '2', name: '9 - 12 tháng' },
     { id: '3', name: '12 - 18 tháng' },
@@ -9,9 +9,10 @@ export const variables = {
     { id: '6', name: '30 - 36 tháng' },
     { id: '7', name: '36 - 50 tháng' },
     { id: '8', name: '50- 60 tháng' },
+    { id: '9', name: '61- 72 tháng' },
   ],
   AGE_TABLE: [
-    { id: '0-6', name: '0 - 6 tháng' }, 
+    { id: '0-6', name: '0 - 6 tháng' },
     { id: '6-9', name: '6 - 9 tháng' },
     { id: '9-12', name: '9 - 12 tháng' },
     { id: '12-18', name: '12 - 18 tháng' },
@@ -20,10 +21,11 @@ export const variables = {
     { id: '30-36', name: '30 - 36 tháng' },
     { id: '36-50', name: '36 - 50 tháng' },
     { id: '50-60', name: '50- 60 tháng' },
+    { id: '61-72', name: '61- 72 tháng' },
   ],
   APPLY: [
     { id: 'inputAssessment', name: 'Test đầu vào' },
     { id: 'periodicAssessment', name: 'Đánh giá định kỳ' },
-  ]
-  };
+  ],
+};
 export default variables;
