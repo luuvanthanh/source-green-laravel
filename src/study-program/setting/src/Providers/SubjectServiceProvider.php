1873bb9571bf2fa7c5b96238cd89557b02a6929f
dangnv <dangnv@greenglobal.vn>
2022-12-02 16:04:00 +0700

    fix(StudyProgramSetting): fix conflict

--- src/study-program/setting/src/Providers/SubjectServiceProvider.php
+++ src/study-program/setting/src/Providers/SubjectServiceProvider.php
@@ -3,15 +3,10 @@
 namespace GGPHP\StudyProgram\Setting\Providers;
 
 use GGPHP\StudyProgram\Setting\Repositories\Contracts\EvaluationCriteriaRepository;
-<<<<<<< HEAD
-use GGPHP\StudyProgram\Setting\Repositories\Contracts\SubjectRepository;
-use GGPHP\StudyProgram\Setting\Repositories\Eloquent\EvaluationCriteriaRepositoryEloquent;
-=======
 use GGPHP\StudyProgram\Setting\Repositories\Contracts\SampleCommentRepository;
 use GGPHP\StudyProgram\Setting\Repositories\Contracts\SubjectRepository;
 use GGPHP\StudyProgram\Setting\Repositories\Eloquent\EvaluationCriteriaRepositoryEloquent;
 use GGPHP\StudyProgram\Setting\Repositories\Eloquent\SampleCommentRepositoryEloquent;
->>>>>>> f26c4c9ce7dd9a40420dc2f9b4ac187684f9ea2b
 use GGPHP\StudyProgram\Setting\Repositories\Eloquent\SubjectRepositoryEloquent;
 use Illuminate\Support\ServiceProvider;
 
@@ -38,9 +33,6 @@ class SubjectServiceProvider extends ServiceProvider
     {
         $this->app->bind(SubjectRepository::class, SubjectRepositoryEloquent::class);
         $this->app->bind(EvaluationCriteriaRepository::class, EvaluationCriteriaRepositoryEloquent::class);
-<<<<<<< HEAD
-=======
         $this->app->bind(SampleCommentRepository::class, SampleCommentRepositoryEloquent::class);
->>>>>>> f26c4c9ce7dd9a40420dc2f9b4ac187684f9ea2b
     }
 }
