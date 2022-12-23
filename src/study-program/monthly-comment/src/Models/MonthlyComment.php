4c9bd4e2b755dd461db4734fd139cee653904ddd
tamnn <nhattam253@gmail.com>
2022-12-23 14:42:22 +0700

    Fix(English): fix filter by script review

--- src/study-program/monthly-comment/src/Models/MonthlyComment.php
+++ src/study-program/monthly-comment/src/Models/MonthlyComment.php
@@ -19,13 +19,19 @@ class MonthlyComment extends UuidModel
     const STATUS = [
         'NOT_REVIEW' => 1,
         'REVIEWED' => 2,
-        'CONFIRMED' => 3,
-        'SENT' => 4
+        'NOT_YET_CONFIRM' => 3,
+        'CONFIRMED' => 4,
+        'NOT_YET_SENT' => 5,
+        'SENT' => 6
+    ];
+
+    const TYPE = [
+        'DUPLICATE' => 2,
     ];
 
     protected $fillable = [
-        'StudentId', 'Status', 'TeacherId', 'TeacherManagementId',
-        'SchoolYearId', 'Month', 'ScriptReviewId'
+        'StudentId', 'ScriptReviewId', 'Status', 'TeacherId', 'TeacherManagementId', 'SchoolYearId',
+        'ReportTime', 'ConfirmationTime', 'Type', 'QuarterReportId', 'Month'
     ];
 
     public function sampleComment()
