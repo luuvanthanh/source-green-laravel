bbd695c3fdbfa60c83fd7893d4d7a85f9425f297
tamnn <nhattam253@gmail.com>
2022-12-29 10:03:24 +0700

    Fix(Shift): update shift

--- src/packages/shift-schedule/src/Http/Requests/ShiftUpdateRequest.php
+++ src/packages/shift-schedule/src/Http/Requests/ShiftUpdateRequest.php
@@ -66,8 +66,11 @@ class ShiftUpdateRequest extends FormRequest
                             return $fail('Thời gian về sớm phải nhỏ hơn thời gian kết thúc.');
                         }
 
-                        if ($value[$i]['startTime'] <= $value[$i - 1]['endTime']) {
-                            return $fail('Thời gian không hợp lệ.');
+                        if ($i >= 1) {
+                            if ($value[$i]['startTime'] <= $value[$i - 1]['endTime']) {
+
+                                return $fail('Thời gian không hợp lệ.');
+                            }
                         }
 
                         if ($value[$i]['startTime'] > $value[$i]['endTime']) {
