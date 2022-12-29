f809da056d51de7e87dc261db33d0b78141d381b
tamnn <nhattam253@gmail.com>
2022-12-29 11:32:57 +0700

    Fix(DB): Fix migration data type

---
+++ src/study-program/quarter-report/database/migrations/2022_12_29_094734_update_time_to_quarter_reports_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTimeToQuarterReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('study-program.QuarterReports', function (Blueprint $table) {
+            $table->dropColumn(['ReportTime', 'ConfirmationTime']);
+        });
+
+        Schema::table('study-program.QuarterReports', function (Blueprint $table) {
+            $table->dateTime('ReportTime')->nullable();
+            $table->dateTime('ConfirmationTime')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('study-program.QuarterReports', function (Blueprint $table) {
+            $table->dropColumn(['SentTime', 'TeacherSentId']);
+        });
+    }
+}
